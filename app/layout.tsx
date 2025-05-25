8bcec37253f3338e1df34f597e4b873849488d47
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-05-25 16:48:31 +0100

    text preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,25 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Nikah Invitation",
-  description: "Humza & Farheen's Nikkah Invitation",
+export const metadata = {
+    title: "Nikah & Walima Invitation – Humza & Farheen",
+    description: "You're cordially invited to the Nikah and Walima ceremony of Humza and Farheen. View details and RSVP online.",
+    openGraph: {
+        title: "Nikah & Walima Invitation – Humza & Farheen",
+        description:
+            "Join us in celebrating the blessed union of Humza & Farheen. Tap to view invitation details, venue, and RSVP.",
+        url: "https://nikah-invitation.vercel.app/",
+        type: "website",
+        images: [
+            {
+                url: "/assets/images/banner.png",
+                width: 1200,
+                height: 630,
+                alt: "Humza & Farheen Nikah Invitation",
+            },
+        ],
+    },
+
 };
 
 export default function RootLayout({
